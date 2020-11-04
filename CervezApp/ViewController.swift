69de00a7d1f0779ff1cfa502f953ab91b535c53c
Ricardo González Pacheco <rigonpa@gmail.com>
2020-11-04 19:58:37 +0100

    Initial Commit

---
+++ CervezApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CervezApp
+//
+//  Created by Ricardo González Pacheco on 04/11/2020.
+//  Copyright © 2020 Ricardo González Pacheco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
